64ebf630d0ed88320306ab90ae381bd2f84c40e3
dando <95683796+LyRiCaLLL@users.noreply.github.com>
2022-11-28 21:52:45 +0200

    dev

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+  hotProductsList.forEach((hotProduct) => {
+    hotProduct.innerHTML += '🔥';
+  });
 });
