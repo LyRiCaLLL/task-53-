082987457b197ca7234afbf168ac39d83fe49caa
dando <95683796+LyRiCaLLL@users.noreply.github.com>
2022-11-28 21:54:35 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
+  const hotProductsList = document.querySelectorAll(".hot");
+
   hotProductsList.forEach((hotProduct) => {
     hotProduct.innerHTML += '🔥';
   });
+
 });
